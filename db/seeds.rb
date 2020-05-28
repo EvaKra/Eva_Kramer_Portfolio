2f3425a2907f44ba51208d4ba41e97b30c473725
eva kramer <eva.kramer91@gmail.com>
2020-05-28 19:08:56 +0100

    seeded two projects, logic projects index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.destroy_all
+puts "deleted old data"
+
+recyclable = Project.create(name: "Recyclable", description:"An app that helps the user to recycle more effectively.", url: "http://www.recyclable.rocks/", github: "https://github.com/EvaKra/recyclable")
+recyclable.photos.attach(io: File.open('app/assets/images/products1.png'), filename: 'products1.png', content_type: 'image/png')
+recyclable.photos.attach(io: File.open('app/assets/images/search1.png'), filename: 'search1.png', content_type: 'image/png')
+recyclable.save!
+puts "recyclable done"
+
+travelizer = Project.create(name: "Travelizer", description:"Platform to share travel itineraries.", url: "https://travelizer-evakra.herokuapp.com/", github: "https://github.com/EvaKra/travelizer")
+travelizer.photos.attach(io: File.open('app/assets/images/Travelizer_search.png'), filename: 'Travelizer_search.png', content_type: 'image/png')
+puts "travelizer done"
+
+puts"all done, fab"
