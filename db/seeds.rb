135c50df16b055c6054c62783cdcf6421110ccdb
eva kramer <eva.kramer91@gmail.com>
2020-05-29 12:52:25 +0100

    image for recyclable

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,12 @@
 Project.destroy_all
 puts "deleted old data"
 
-travelizer = Project.create(name: "Travelizer", description:"A new web application for travel enthusiasts, a platform to plan, create and share travel itineraries.", url: "https://travelizer-evakra.herokuapp.com/", github: "https://github.com/EvaKra/travelizer")
+travelizer = Project.create(name: "Travelizer", description:"A new web application for travel enthusiasts. Users can visit the itinerary library to find inspiration for their next trip and create and share their own travel itineraries with the Travelizer community.", url: "https://travelizer-evakra.herokuapp.com/", github: "https://github.com/EvaKra/travelizer")
 travelizer.photos.attach(io: File.open('app/assets/images/Travelizer_grouped.png'), filename: 'Travelizer_grouped.png', content_type: 'image/png')
 puts "travelizer done"
 
-recyclable = Project.create(name: "Recyclable", description:"Recycling household waste can be confusing. This app helps users make quick and effective decisions.", url: "http://www.recyclable.rocks/", github: "https://github.com/EvaKra/recyclable")
-recyclable.photos.attach(io: File.open('app/assets/images/products1.png'), filename: 'products1.png', content_type: 'image/png')
-recyclable.photos.attach(io: File.open('app/assets/images/search1.png'), filename: 'search1.png', content_type: 'image/png')
+recyclable = Project.create(name: "Recyclable", description:"Recycling household waste can be confusing. This app helps users make quick and effective decisions and also includes a quiz for users to up their recycling game.", url: "http://www.recyclable.rocks/", github: "https://github.com/EvaKra/recyclable")
+recyclable.photos.attach(io: File.open('app/assets/images/Recyclable3.png'), filename: 'Recyclable.png', content_type: 'image/png')
 recyclable.save!
 puts "recyclable done"
 
